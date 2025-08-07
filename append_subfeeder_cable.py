c1461ed00d006c204093ac562d1ac04a32a05725
ahmadtara <ahmadtara.pratama@gmail.com>
2025-08-07 11:07:24 +0700

    Update and rename sheet_utils.py to append_subfeeder_cable.py

---
+++ append_subfeeder_cable.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+def append_subfeeder_cable(sheet, cable_data, district, subdistrict, vendor, kmz_name):
+    rows = []
+    template_row = existing_rows[-1] if len(existing_rows) > 1 else []
+ 
+        row[0] = name
+        row[1] = name
+        row[3:5] = template_row[3:5]
+        row[20:21] = template_row[20:21]
+        row[24] = datetime.today().strftime("%d/%m/%Y")
+        
+        rows.append(row)
+
+    sheet.append_rows(rows, value_input_option="USER_ENTERED")
+    return len(rows)
