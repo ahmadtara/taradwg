a987258ff7fd7ba950f12a96f2ad1096de6e180f
ahmadtara <ahmadtara.pratama@gmail.com>
2025-08-07 11:12:35 +0700

    Update append_subfeeder_cable.py

--- append_subfeeder_cable.py
+++ append_subfeeder_cable.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
 def append_subfeeder_cable(sheet, cable_data, district, subdistrict, vendor, kmz_name):
+    existing_rows = sheet.get_all_values()
     rows = []
     template_row = existing_rows[-1] if len(existing_rows) > 1 else []
- 
+        row = [""] * len(existing_rows[0])
         row[0] = name
         row[1] = name
         row[3:5] = template_row[3:5]
